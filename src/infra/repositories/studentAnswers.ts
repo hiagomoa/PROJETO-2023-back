d366df9dec6155c97f382100c9f48c417a75fa1c
Lucas Laurentino <laurentino14@outlook.com>
2023-10-31 23:01:01 -0300

    fix: type errors

--- src/infra/repositories/studentAnswers.ts
+++ src/infra/repositories/studentAnswers.ts
@@ -5,9 +5,19 @@ import { StudentAnswer } from "../../domain/entities/studentAnswer";
 
 export class StudentAnswerRepo implements StudentAnswerRepository {
   constructor(private readonly db: PrismaClient) {}
-  async create(studentAnswer: StudentAnswer): Promise<void> {
+  async create(studentAnswer: {
+    studentId: string;
+    exerciseId: string;
+    answer: string;
+    attempts: number | string;
+  }): Promise<void> {
     const db = await this.db.studentAnswer.create({
-      data: studentAnswer,
+      data: {
+        studentId: studentAnswer.studentId,
+        exerciseId: studentAnswer.exerciseId,
+        answer: studentAnswer.answer,
+        attempts: Number(studentAnswer.attempts),
+      },
     });
     if (!db) {
       throw new Error("Erro ao criar resposta do estudante");
@@ -41,7 +51,11 @@ export class StudentAnswerRepo implements StudentAnswerRepository {
   }
   async updateStudentAnswer(
     id: string,
-    studentAnswer: StudentAnswer
+    studentAnswer: {
+      studentId: string;
+      exerciseId: string;
+      answer: string;
+    }
   ): Promise<void> {
     const db = await this.db.studentAnswer.update({
       where: { id },
